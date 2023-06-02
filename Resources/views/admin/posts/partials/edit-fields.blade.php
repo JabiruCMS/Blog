24e8639577150e7cd680ff135841ed24d7178179
David Cap <david.cap@bocians.net>
2023-06-01 22:42:26 -0600

    limix max length of blog post inputs to 191 characters

--- Resources/views/admin/posts/partials/edit-fields.blade.php
+++ Resources/views/admin/posts/partials/edit-fields.blade.php
@@ -2,13 +2,13 @@
     <div class='form-group{{ $errors->has("$lang.title") ? ' has-error' : '' }}'>
         <?php $oldTitle = isset($post?->translate($lang)->title) ? $post->translate($lang)->title : ''; ?>
         {!! Form::label("{$lang}[title]", trans('blog::post.form.title')) !!}
-        {!! Form::text("{$lang}[title]", old("$lang.title", $oldTitle), ['class' => 'form-control', 'data-slug' => 'source', 'placeholder' => trans('blog::post.form.title')]) !!}
+        {!! Form::text("{$lang}[title]", old("$lang.title", $oldTitle), ['class' => 'form-control', 'maxlength' => '190', 'data-slug' => 'source', 'placeholder' => trans('blog::post.form.title')]) !!}
         {!! $errors->first("$lang.title", '<span class="help-block">:message</span>') !!}
     </div>
     <div class='form-group{{ $errors->has("$lang.slug") ? ' has-error' : '' }}'>
         <?php $oldSlug = isset($post?->translate($lang)->slug) ? $post->translate($lang)->slug : ''; ?>
         {!! Form::label("{$lang}[slug]", trans('blog::post.form.slug')) !!}
-        {!! Form::text("{$lang}[slug]", old("$lang.slug", $oldSlug), ['class' => 'form-control slug', 'data-slug' => 'target', 'placeholder' => trans('blog::post.form.slug')]) !!}
+        {!! Form::text("{$lang}[slug]", old("$lang.slug", $oldSlug), ['class' => 'form-control slug', 'maxlength' => '190', 'data-slug' => 'target', 'placeholder' => trans('blog::post.form.slug')]) !!}
         {!! $errors->first("$lang.slug", '<span class="help-block">:message</span>') !!}
     </div>
     <?php $old = isset($post?->translate($lang)->content) ? $post->translate($lang)->content : ''; ?>
@@ -40,6 +40,7 @@
                             old("$lang.{$fieldName}", $oldValue),
                             [
                                 'class' => 'form-control',
+                                'maxlength' => '190',
                                 'placeholder' => trans("blog::post.form.{$fieldName}")
                             ]
                         ) !!}
